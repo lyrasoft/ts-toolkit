7d7ab47fdc24799ef4895d1ad8908cda86fb0597
Simon Asika <asika32764@gmail.com>
2025-01-31 15:20:35 +0800

    misc updates

--- src/sweetalert.ts
+++ src/sweetalert.ts
@@ -1,23 +1,78 @@
 import swal from 'sweetalert';
-export function simpleAlert(title: string, text: string = '', icon: string = 'info') {
-  return swal({
+
+type SwalParams = Exclude<Parameters<typeof swal>[0], string>;
+
+export function simpleAlert(
+  title: string,
+  text: string = '',
+  icon: string = 'info',
+  extra?: Partial<SwalParams>
+) {
+  const params = {
     title,
     text,
     icon,
     buttons: [
-      '確認'
-    ]
-  });
+      '確認',
+    ],
+  };
+
+  if (extra) {
+    Object.assign(params, extra);
+  }
+
+  return swal(params);
 }
 
-export function simpleConfirm(title: string, text: string = '', icon: string = 'info') {
-  return swal({
+export function simpleConfirm(
+  title: string,
+  text: string = '',
+  icon: string = 'info',
+  extra?: Partial<SwalParams>,
+) {
+  const params = {
     title,
     text,
     icon,
     buttons: [
       '取消',
       '確認',
-    ]
-  })
+    ],
+  };
+
+  if (extra) {
+    Object.assign(params, extra);
+  }
+
+  return swal(params);
+}
+
+export function deleteConfirm(
+  title: string,
+  text: string = '',
+  icon: string = 'info',
+  extra?: Partial<SwalParams>,
+) {
+  const params = {
+    title,
+    text,
+    icon,
+    buttons: {
+      cancel: {
+        text: '取消',
+        value: false,
+        visible: true
+      },
+      delete: {
+        text: '刪除',
+        value: true,
+      },
+    },
+  };
+
+  if (extra) {
+    Object.assign(params, extra);
+  }
+
+  return swal(params);
 }
