5b50db1b56bfc66b06e9dbccfd0c9f6592a0d149
Simon Asika <asika32764@gmail.com>
2025-02-11 22:06:55 +0800

    Prepare release.

--- bin/release.mjs
+++ bin/release.mjs
@@ -35,8 +35,8 @@ try {
   console.log(e.message);
 }
 
-console.log(`>>> npm version ${args.join(' ')}`);
-exec(`npm version ${args.join(' ')}`);
+console.log(`>>> yarn version ${args.join(' ')}`);
+exec(`yarn version ${args.join(' ')}`);
 
 const branch = cliInput['b'] || 'main';
 
