347d99312af17d64b47e706ca51b6afe6423e31b
Simon Asika <asika32764@gmail.com>
2026-02-17 17:07:05 +0800

    Make loading run() wrap() can set min loading time #12

--- src/vue/loading.ts
+++ src/vue/loading.ts
@@ -1,11 +1,23 @@
 import { type Ref, ref } from 'vue';
-import { simpleAlert } from '../generic';
+import { simpleAlert, sleep } from '../generic';
+
+type LoadingRunOptions = {
+  errorAlert?: boolean;
+  minLoadingTime?: number;
+} | boolean;
 
 export function useLoading(loading?: Ref<boolean>) {
   loading = loading || ref(false);
 
-  const run = async function <T>(callback: () => Promise<T>, errorAlert = true): Promise<T> {
+  const run = async function <T>(callback: () => Promise<T>, options: LoadingRunOptions = {}): Promise<T> {
     loading!.value = true;
+
+    const handledOptions = typeof options === 'boolean' ? { errorAlert: options } : options;
+
+    const errorAlert = handledOptions.errorAlert ?? true;
+    const minLoadingTime = handledOptions.minLoadingTime ?? 0;
+
+    const startTime = Date.now();
 
     try {
       return await callback();
@@ -16,16 +28,22 @@ export function useLoading(loading?: Ref<boolean>) {
       }
       throw e;
     } finally {
+      const elapsedTime = Date.now() - startTime;
+
+      if (elapsedTime < minLoadingTime) {
+        await sleep(minLoadingTime - elapsedTime);
+      }
+
       loading!.value = false;
     }
   }
 
   const wrap = function <T, K extends ((...args: any[]) => any)>(
     callback: K,
-    errorAlert = true
+    options: LoadingRunOptions = {}
   ): (...args: Parameters<K>) => Promise<ReturnType<K>> {
     return (...args: any[]) => {
-      return run(async () => callback(...args), errorAlert);
+      return run(async () => callback(...args), options);
     };
   };
 
