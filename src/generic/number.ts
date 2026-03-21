028be33a7105b46b40fe80da60824b0276a0b5d8
Simon Asika <asika32764@gmail.com>
2026-03-21 23:31:50 +0800

    Fix numberFormat

--- src/generic/number.ts
+++ src/generic/number.ts
@@ -1,14 +1,21 @@
 export function numberFormat(number: string | number, decimals = 0, decPoint = '.', thousandsSep = ',') {
   number = Number(number);
 
-  const str = number.toFixed(decimals ? decimals : 0).toString().split('.');
+  const sign = number < 0 ? '-' : '';
+  number = Math.abs(number);
+
+  const str: string[] = number.toFixed(decimals ? decimals : 0).toString().split('.');
   const parts = [];
 
-  for (var i = str[0].length; i > 0; i -= 3) {
+  if (!str[0]) {
+    return '0';
+  }
+
+  for (let i = str[0].length; i > 0; i -= 3) {
     parts.unshift(str[0].substring(Math.max(0, i - 3), i));
   }
 
   str[0] = parts.join(thousandsSep ? thousandsSep : ',');
 
-  return str.join(decPoint ? decPoint : '.');
+  return sign + str.join(decPoint ? decPoint : '.');
 }
